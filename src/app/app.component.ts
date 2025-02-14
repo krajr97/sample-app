4f13eec7756125a4748cb8a660fe5e0e14f76391
Raj Sharma <krajr97@gmail.com>
2025-02-14 13:22:59 +0530

    dev: Setup primeNG and Add GitHub Actions for CI/CD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PRIMENG_MODULES } from './primeng-imports';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [...PRIMENG_MODULES],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'sample-app';
-}
+export class AppComponent {}
---
+++ src/app/primeng-imports.ts
@@ -0,0 +1,4 @@
+// PrimeNG Modules
+import { ButtonModule } from 'primeng/button';
+
+export const PRIMENG_MODULES = [ButtonModule];
